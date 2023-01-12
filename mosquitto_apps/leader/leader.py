aebc506a42dc1aef21093fae6f2df7bfbf4eecab
Florian Bauernfeind <e11775814@student.tuwien.ac.at>
2023-01-12 14:05:34 +0100

    receiving data by a another docker container now works

--- mosquitto_apps/leader/leader.py
+++ mosquitto_apps/leader/leader.py
@@ -4,9 +4,10 @@ import sys
 
 class LeaderNode():
 
-    def __init__(self, data_set_type):
+    def __init__(self, data_set_type, host_address):
 
         self.client = None
+        self.host_address = host_address
         self.data_set_type = data_set_type
         self.set_up_connection()
 
@@ -21,11 +22,16 @@ class LeaderNode():
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
-        self.client.connect("localhost", 1883, 60)
+        self.client.connect(host_address, 1883, 60)
         self.client.loop_forever()
 
 
 if __name__ == "__main__":
 
     data_set_type = sys.argv[1]
-    LeaderNode(data_set_type)
+    if len(sys.argv) >= 3:
+        host_address = sys.argv[2]
+    else:
+        host_address = "localhost"
+
+    LeaderNode(data_set_type, host_address)
