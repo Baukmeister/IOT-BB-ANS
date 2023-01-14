dc7d5c531aee0589c87aa7027dac1a46febaba43
floba <flo.bauernfeind@gmail.com>
2023-01-14 14:02:52 +0100

    refactor neural compressor

--- compression/Neural_Compressor.py
+++ compression/Neural_Compressor.py
@@ -16,11 +16,14 @@ from models.vanilla_vae import Vanilla_VAE
 from util import bb_util
 from util.io import vae_model_name
 
-# TODO rework to offer interface for docker deployment
-class NeuralCompressor():
 
-    def __init__(self, params: Params, data_samples: list, name: str, input_dim):
+# TODO rework to offer interface for docker deployment
+class NeuralCompressor:
+
+    def __init__(self, params: Params, data_samples: list, name: str, input_dim, plot=False):
+        self.plot = plot
         self.params = params
+        self.state = None
 
         self.data_samples = data_samples
         self.name = name
@@ -35,7 +38,7 @@ class NeuralCompressor():
             self.params.scale_factor,
             self.params.model_type,
             self.params.shift,
-            data_set_type =self.params.metric
+            data_set_type=self.params.metric
         )
 
         self.num_batches = len(self.data_samples) // self.params.compression_batch_size
@@ -79,7 +82,7 @@ class NeuralCompressor():
         elif self.params.model_type == "vanilla_vae":
             print("Using Vanilla VAE")
             self.model = vanilla_vae
-            #TODO: add this
+            # TODO: add this
             obs_append = None
             obs_pop = None
         elif self.params.model_type == "beta_binomial_vae":
@@ -99,56 +102,60 @@ class NeuralCompressor():
         # set up compression methods
         latent_shape = (self.params.compression_batch_size, self.params.latent_dim)
 
-
         self.vae_append = bb_util.vae_append(latent_shape, gen_net, rec_net, obs_append,
-                                        self.params.prior_precision, self.params.q_precision)
+                                             self.params.prior_precision, self.params.q_precision)
         self.vae_pop = bb_util.vae_pop(latent_shape, gen_net, rec_net, obs_pop,
-                                  self.params.prior_precision, self.params.q_precision)
+                                       self.params.prior_precision, self.params.q_precision)
+        self.rng = numpy.random.RandomState()
+        self.other_bits = self.rng.randint(low=1 << 16, high=1 << 31, size=50, dtype=np.uint32)
+        self.state = rans.unflatten(self.other_bits)
+        self.stack_sizes = []
+
 
 
     def run_compression(self):
-        rng = numpy.random.RandomState()
 
-        other_bits = rng.randint(low=1 << 16, high=1 << 31, size=50, dtype=np.uint32)
-        state = rans.unflatten(other_bits)
-        data_points = np.split(np.reshape(self.data_samples, (len(self.data_samples), -1)), self.num_batches)
+        data_points = self.encode_data_set()
+        self.decode_entire_state(data_points)
 
-        stack_sizes = []
+        if self.plot:
+            # plot_stack size:
+            x = list(range(len(self.stack_sizes)))
+            plt.plot(x, self.stack_sizes)
+            plt.title(f"Stack size per per sample")
+            plt.show()
 
-        encode_start_time = time.time()
-        for i, data_point in tqdm(enumerate(data_points), total=self.params.compression_samples_num):
-            state = self.vae_append(state, data_point)
-
-            stack_sizes.append((stack_depth(state)))
-
-        print('\nAll encoded in {:.2f}s'.format(time.time() - encode_start_time))
-        compressed_message = rans.flatten(state)
-
-        compressed_bits = 32 * (len(compressed_message) - len(other_bits))
-        compression_rate = compressed_bits/(np.size(data_points)*32)
-        bits_per_datapoint = compressed_bits / np.size(data_points)
-        print("Used " + str(compressed_bits) + " bits.")
-        print(f'Compression ratio: {round(compression_rate, 4)}. BpD: {bits_per_datapoint}')
-
-
-        state = rans.unflatten(compressed_message)
+    def decode_entire_state(self, data_points):
         decode_start_time = time.time()
         reconstructed_data_points = []
-
         print("\nDecoding data points ...")
-        for n in tqdm(range(len(data_points))):
-            state, data_point_ = self.vae_pop(state)
-            reconstructed_data_points.insert(0, data_point_.numpy()[0])
-
+        for _ in tqdm(range(len(data_points))):
+            self.remove_from_state(reconstructed_data_points)
         print('\nAll decoded in {:.2f}s'.format(time.time() - decode_start_time))
-
-        recovered_bits = rans.flatten(state)
-        assert all(other_bits == recovered_bits)
+        recovered_bits = rans.flatten(self.state)
+        assert all(self.other_bits == recovered_bits)
         np.testing.assert_equal(reconstructed_data_points, self.data_samples)
         print('\nLossless reconstruction!')
 
-        # plot_stack size:
-        x = list(range(len(stack_sizes)))
-        plt.plot(x, stack_sizes)
-        plt.title(f"Stack size per per sample")
-        plt.show()
+    def encode_data_set(self):
+        data_points = np.split(np.reshape(self.data_samples, (len(self.data_samples), -1)), self.num_batches)
+        encode_start_time = time.time()
+        for i, data_point in tqdm(enumerate(data_points), total=self.params.compression_samples_num):
+            self.add_to_state(data_point)
+        print('\nAll encoded in {:.2f}s'.format(time.time() - encode_start_time))
+        compressed_message = rans.flatten(self.state)
+        compressed_bits = 32 * (len(compressed_message) - len(self.other_bits))
+        compression_rate = compressed_bits / (np.size(data_points) * 32)
+        bits_per_datapoint = compressed_bits / np.size(data_points)
+        print("Used " + str(compressed_bits) + " bits.")
+        print(f'Compression ratio: {round(compression_rate, 4)}. BpD: {bits_per_datapoint}')
+        self.state = rans.unflatten(compressed_message)
+        return data_points
+
+    def remove_from_state(self, reconstructed_data_points):
+        self.state, data_point_ = self.vae_pop(self.state)
+        reconstructed_data_points.insert(0, data_point_.numpy()[0])
+
+    def add_to_state(self, data_point):
+        self.state = self.vae_append(self.state, data_point)
+        self.stack_sizes.append((stack_depth(self.state)))
