76fda70fff2781e60f394802aeca49af5b4ea9a1
floba <flo.bauernfeind@gmail.com>
2023-02-01 17:31:41 +0100

    add todo

--- mosquitto_apps/build_and_push.sh
+++ mosquitto_apps/build_and_push.sh
@@ -1,3 +1,7 @@
 docker-compose build
 docker push baukmeister/leader-app
 docker push baukmeister/sensor-app
+
+scp -r .\edgebench_scripts\leader\* jetson:leader-scripts
+scp -r .\edgebench_scripts\sensors\* nuc:sensor-scripts
+
--- mosquitto_apps/edgebench_scripts/sensors/benchmark_sensor_sample_nums/run_sensors_benchmark.sh
+++ mosquitto_apps/edgebench_scripts/sensors/benchmark_sensor_sample_nums/run_sensors_benchmark.sh
@@ -1,74 +1,76 @@
+#TODO: Figure this out "FileNotFoundError: [Errno 2] No such file or directory: 'params/benchmark_compression_eval/10_samples/simple.json'"
+
 echo "1 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1_samples/simple.json -1 benchmark
 
 echo "5 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/household.json -1  benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/5_samples/simple.json -1 benchmark
 
 
 echo "10 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/10_samples/simple.json -1 benchmark
 
 
 echo "20 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/20_samples/simple.json -1 benchmark
 
 
 echo "30 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/30_samples/simple.json -1 benchmark
 
 
 echo "40 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/40_samples/simple.json -1 benchmark
 
 
 echo "50 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/50_samples/simple.json -1 benchmark
 
 
 echo "100 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/100_samples/simple.json -1 benchmark
 
 
 echo "500 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/ 500_samples/simple.json -1 benchmark
 
 
 echo "1000 Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/1000_samples/simple.json -1 benchmark
 
 
 echo "MAX Samples"
-docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/household.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/intel.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/wisdm.json -1
-docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/simple.json -1
+docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/household.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/intel.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/wisdm.json -1 benchmark
+docker run baukmeister/sensor-app params/benchmark_compression_eval/max_samples/simple.json -1 benchmark
